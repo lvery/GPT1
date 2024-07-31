6ce2ecee7fce88c33cf16afcbd314215cc5f8984
lvery <107117065+lvery@users.noreply.github.com>
2024-07-31 12:45:43 +0800

    exporter.tsx

--- app/components/exporter.tsx
+++ app/components/exporter.tsx
@@ -539,7 +539,7 @@ export function ImagePreviewer(props: {
           </div>
 
           <div>
-            <div className={styles["main-title"]}>NextChat</div>
+            <div className={styles["main-title"]}>轻颜智慧</div>
             <div className={styles["sub-title"]}>
               github.com/ChatGPTNextWeb/ChatGPT-Next-Web
             </div>
